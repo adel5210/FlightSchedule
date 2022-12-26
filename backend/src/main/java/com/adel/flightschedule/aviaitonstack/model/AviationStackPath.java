d49343b678bf0569553475bfbb3229f00fab5549
adel <adel.albediwy@gmail.com>
2022-12-26 08:27:32 +0300

    Minor commits

---
+++ backend/src/main/java/com/adel/flightschedule/aviaitonstack/model/AviationStackPath.java
@@ -0,0 +1,21 @@
+package com.adel.flightschedule.aviaitonstack.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpMethod;
+
+@Getter
+@AllArgsConstructor
+public enum AviationStackPath {
+
+    FLIGHTS("flights", HttpMethod.GET),
+    ROUTES("routes", HttpMethod.GET),
+    AIRPORTS("airports", HttpMethod.GET),
+    AIRLINES("airlines", HttpMethod.GET),
+
+    ;
+
+    private final String path;
+    private final HttpMethod requestType;
+
+}
---
+++ backend/src/main/java/com/adel/flightschedule/aviaitonstack/service/AviationStackPathsFactory.java
@@ -0,0 +1,10 @@
+package com.adel.flightschedule.aviaitonstack.service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AviationStackPathsFactory {
+
+
+
+}
