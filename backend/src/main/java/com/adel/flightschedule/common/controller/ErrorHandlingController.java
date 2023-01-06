568a10efc92970ea98ed08fe6e4f2c7d92053b4c
adel <adel.albediwy@gmail.com>
2023-01-06 09:12:24 +0300

    Added  error controller

--- backend/src/main/java/com/adel/flightschedule/common/controller/ErrorHandlingController.java
+++ backend/src/main/java/com/adel/flightschedule/common/controller/ErrorHandlingController.java
@@ -1,7 +1,10 @@
 package com.adel.flightschedule.common.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,8 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ErrorHandlingController implements ErrorController {
 
     @RequestMapping("/error")
-    public ResponseEntity<String> handleError(){
-        return new ResponseEntity<>("Not Found", HttpStatusCode.valueOf(200));
+    public ResponseEntity<String> handleError(final HttpServletRequest request){
+        final Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(null!=status){
+            final Integer statusCode = Integer.valueOf(status.toString());
+            if(statusCode.equals(HttpStatus.NOT_FOUND.value())){
+                return new ResponseEntity<>("Not Found", HttpStatusCode.valueOf(statusCode));
+            }else if(statusCode.equals(HttpStatus.INTERNAL_SERVER_ERROR.value())){
+                return new ResponseEntity<>("Internal error", HttpStatusCode.valueOf(statusCode));
+            }
+        }
+
+        return new ResponseEntity<>("Unknown", HttpStatusCode.valueOf(404));
     }
 
 }
