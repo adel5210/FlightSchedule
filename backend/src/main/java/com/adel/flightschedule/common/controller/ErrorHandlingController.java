66605e7c3f7cad99c4304e8432e09bda476d2866
adel <adel.albediwy@gmail.com>
2023-01-06 08:59:20 +0300

    Added  error controller

---
+++ backend/src/main/java/com/adel/flightschedule/common/controller/ErrorHandlingController.java
@@ -0,0 +1,19 @@
+package com.adel.flightschedule.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ErrorHandlingController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<String> handleError(){
+        return new ResponseEntity<>("Not Found", HttpStatusCode.valueOf(404));
+    }
+
+}
