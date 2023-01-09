30a502dcddd6873e96e97fc71b7351643fddb603
adel <adel.albediwy@gmail.com>
2023-01-09 16:07:17 +0300

    Minor fix

--- backend/src/main/java/com/adel/flightschedule/common/controller/GlobalControllerAdvisor.java
+++ backend/src/main/java/com/adel/flightschedule/common/controller/GlobalControllerAdvisor.java
@@ -8,21 +8,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@RestControllerAdvice
-@Slf4j
+//@RestControllerAdvice
+//@Slf4j
 public class GlobalControllerAdvisor {
-
-    @ExceptionHandler(value = {
-            Exception.class
-    })
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleGlobalException(Exception ex, WebRequest request) {
-        final ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(HttpStatus.BAD_REQUEST.value())
-                .message(ex.getMessage())
-                .build();
-        log.info(errorResponse.toString());
-        return errorResponse;
-    }
+//
+//    @ExceptionHandler(value = {
+//            Exception.class
+//    })
+//    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//    public ErrorResponse handleGlobalException(Exception ex, WebRequest request) {
+//        final ErrorResponse errorResponse = ErrorResponse.builder()
+//                .code(HttpStatus.BAD_REQUEST.value())
+//                .message(ex.getMessage())
+//                .build();
+//        log.info(errorResponse.toString());
+//        return errorResponse;
+//    }
 
 }
